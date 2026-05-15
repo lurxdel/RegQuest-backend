33addd12599829c642ab0056170a6d890a2f5351
nacht1234 <ralphperoch1789@gmail.com>
2026-05-15 19:04:28 +0800

    feat(requests): implement role-based serializer validation for audit and status fields

--- requests/serializers.py
+++ requests/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Request
+from accounts.models import User
 
 class RequestSerializer(serializers.ModelSerializer):
     document_name = serializers.CharField(source='document_type.document_name', read_only=True)
@@ -7,4 +8,19 @@ class RequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Request
         fields = '__all__'
-        read_only_fields = ['user', 'status', 'created_at', 'updated_at', 'processed_by', 'tracking_number']
+        read_only_fields = ['user','tracking_number', 'created_at', 'updated_at',]
+        
+    def validate(self, attrs):
+        request = self.context.get('request')
+
+        if request and hasattr(request, 'user') and request.user.is_authenticated:
+            if request.user.role == User.Roles.STUDENT:
+                restricted_fields = ['status','processed_by', 'processed_at', 'est_release_date']
+                
+                for field in restricted_fields:
+                    if field in attrs:
+                        raise serializers.ValidationError({field: "You do not have permission to modify this field."})
+
+        return super().validate(attrs)
+
+        
