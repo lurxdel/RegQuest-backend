df5f0f3cb6f18bc20651416a3c9052d10acf41eb
nacht1234 <ralphperoch1789@gmail.com>
2026-05-17 17:06:33 +0800

    refactor: rename requests app to credential_requests to fix namespace collision

--- _core/settings.py
+++ _core/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'accounts',
     'documents',
-    'requests',
+    'credential_requests',
     'corsheaders',
 ]
 
--- _core/urls.py
+++ _core/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('accounts.urls')),
     path('api/v1/documents/', include('documents.urls')),
-    path('api/v1/requests/', include('requests.urls')),
+    path('api/v1/requests/', include('credential_requests.urls')),
 ]
