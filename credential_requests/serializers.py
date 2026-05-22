76aafc727d0778467f2974a2163e6879f86c5d69
Gabecx <jessicagabica54@gmail.com>
2026-05-22 21:52:29 +0800

    update on model and serializers from credential_requests

--- credential_requests/serializers.py
+++ credential_requests/serializers.py
@@ -3,12 +3,15 @@ from .models import Request
 from accounts.models import User
 
 class RequestSerializer(serializers.ModelSerializer):
+    student_name = serializers.SerializerMethodField()
     document_name = serializers.CharField(source='document_type.document_name', read_only=True)
     processing_time_days = serializers.IntegerField(source='document_type.processing_time_days', read_only=True)
     class Meta:
         model = Request
         fields = '__all__'
         read_only_fields = ['user','tracking_number', 'created_at', 'updated_at',]
+    def get_student_name(self, obj):
+        return obj.user.get_full_name() or obj.user.username
         
     def validate(self, attrs):
         request = self.context.get('request')
